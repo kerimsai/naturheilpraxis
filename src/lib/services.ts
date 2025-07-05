80e6bb3378e6c67325c757ec720fab4e7881fda4
Kerims AI <kerimsaiacc@gmail.com>
2025-07-05 09:34:52 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="flex-g

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -27,8 +27,8 @@ export const services: Service[] = [
   {
     slug: "aderlass-blutreinigung",
     icon: ShieldCheck,
-    title: "Aderlass & Blutreinigung",
-    description: "Traditionelles Ausleitverfahren zur Entgiftung und Blutreinigung.",
+    title: "Aderlass",
+    description: "Traditionelles Ausleitverfahren zur Unterstützung der körpereigenen Regeneration.",
     longDescription: "Der Aderlass nach Hildegard von Bingen ist ein bewährtes Ausleitungsverfahren zur Reinigung des Blutes und zur Entlastung des Körpers. Es dient der Entgiftung, der Verbesserung der Blutzirkulation und der Harmonisierung der Körpersäfte. Diese Behandlung kann bei einer Vielzahl von chronischen Beschwerden und zur allgemeinen Prävention eingesetzt werden.",
     price: "Sitzung: ab 95€",
     image: "https://placehold.co/800x600.png",
