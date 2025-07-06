d11f587ad340545790deb34e6e02d978ac56aa22
Kerims AI <kerimsaiacc@gmail.com>
2025-07-06 19:05:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import { Toaster } from "@/components/ui/toaster"
 export const metadata: Metadata = {
   title: 'Naturheilpraxis Julia Jordan',
   description: 'Ihr Weg zu natürlicher Heilung und Wohlbefinden',
-  icons: null,
 };
 
 export default function RootLayout({
@@ -21,15 +20,15 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
-        <Script 
-          src="https://cloud.ccm19.de/app.js?apiKey=a572c08ee1bde113afedeccb5f01232260a351e2f657c039&domain=686abba124ec8a7a000ee4c2" 
-          referrerPolicy="origin"
-          strategy="beforeInteractive"
-        />
       </head>
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <Script 
+          src="https://cloud.ccm19.de/app.js?apiKey=a572c08ee1bde113afedeccb5f01232260a351e2f657c039&domain=686abba124ec8a7a000ee4c2" 
+          referrerPolicy="origin"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
